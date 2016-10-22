17c94bcaba40cdecfa713ed70db1a63062568788
Michał Piechota <mchpiechota@gmail.com>
2016-10-22 20:18:05 +0200

    emergency push 4 Kacper

--- src/main/java/com/jbd/JBDemail.java
+++ src/main/java/com/jbd/JBDemail.java
@@ -4,10 +4,15 @@ public class JBDemail {
     public static void main(String[] args) {
 
         QuestionForm askQuestionsAndQuestionForm = new QuestionForm();
-        askQuestionsAndQuestionForm.askQuestionsAndSetAnswers();
+      //  askQuestionsAndQuestionForm.askQuestionsAndSetAnswers();
+        GetFromDataSubjectContent lol = new GetFromDataSubjectContent();
+
 
         PathGetter pG = new PathGetter();
-        pG.createFileListFromPath(pG.askUserAboutInputPath());
+      //  pG.createFileListFromPath(pG.askUserAboutInputPath());
+
+        String a2 =lol.reportFrom(pG.askUserAboutInputPath());
+        System.out.println(a2);
 
     }
 }
