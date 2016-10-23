93228ca525d1fd6ad9dc0e5e732b773eb5018e3f
Kacper <kacper.rabczak@gmail.com>
2016-10-23 03:53:59 +0200

    added code responsible for program flow

--- src/main/java/com/jbd/JBDemail.java
+++ src/main/java/com/jbd/JBDemail.java
@@ -1,18 +1,28 @@
 package com.jbd;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JBDemail {
     public static void main(String[] args) {
+        List<String> filesInStrings = new ArrayList<>();
+        List<Email> eMailKeeper = new ArrayList<>();
 
         QuestionForm askQuestionsAndQuestionForm = new QuestionForm();
-      //  askQuestionsAndQuestionForm.askQuestionsAndSetAnswers();
-        GetFromDataSubjectContent lol = new GetFromDataSubjectContent();
-
+        //askQuestionsAndQuestionForm.askQuestionsAndSetAnswers();
 
         PathGetter pG = new PathGetter();
-      //  pG.createFileListFromPath(pG.askUserAboutInputPath());
+        //pG.createFileListFromPath(pG.askUserAboutInputPath());
 
-        String a2 =lol.reportFrom(pG.askUserAboutInputPath());
-        System.out.println(a2);
+        FileLoad fL = new FileLoad();
+        //filesInStrings.addAll(pG.getFileList().stream().map(fL::fileLoad).collect(Collectors.toList()));
+        /*Comment left intentionally for educational purposes as explanation of line above
+        for (String s : pG.getFileList()) {
+            filesInStrings.add(fL.fileLoad(s));
+        }*/
+
+        MakeEmailsFromString makeEmails = new MakeEmailsFromString();
+        System.out.println(makeEmails.tempName());
 
     }
 }
