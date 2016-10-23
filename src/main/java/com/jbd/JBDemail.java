4ff8a0e79e0c9c7d314ad391682d5914affc2244
Kacper <kacper.rabczak@gmail.com>
2016-10-23 13:51:20 +0200

    update of main - usage of other features

--- src/main/java/com/jbd/JBDemail.java
+++ src/main/java/com/jbd/JBDemail.java
@@ -10,25 +10,44 @@ public class JBDemail {
         List<Email> eMailKeeper = new ArrayList<>();
 
         QuestionForm askQuestionsAndQuestionForm = new QuestionForm();
-        //askQuestionsAndQuestionForm.askQuestionsAndSetAnswers();
+        askQuestionsAndQuestionForm.askQuestionsAndSetAnswers();
 
         PathGetter pG = new PathGetter();
         pG.createFileListFromPath(pG.askUserAboutInputPath());
 
         FileLoad fL = new FileLoad();
         filesInStrings.addAll(pG.getFileList().stream().map(fL::fileLoad).collect(Collectors.toList()));
-        /*Comment left intentionally for educational purposes as explanation of line above
-        for (String s : pG.getFileList()) {
-            filesInStrings.add(fL.fileLoad(s));
-        }*/
 
         MakeEmailsFromString makeEmails = new MakeEmailsFromString();
         for (String s : filesInStrings) {
             eMailKeeper.addAll(makeEmails.makeEmailList(s));
         }
 
-        System.out.println(eMailKeeper.size());
+        ContentmentVerification cV = new ContentmentVerification();
+
+        List<String> eMailToLookFor = new ArrayList<>();
+        eMailToLookFor.add(SearchCriteria.getEMAIL());
+        List<Email> result1;
+        List<Email> result2;
+        //List<Email> result3;
+        result1 = cV.searchEmailByName(eMailToLookFor, eMailKeeper);
+        result2 = cV.searchEmailByTitleWithKeyWords(SearchCriteria.getKEYWORDS(), eMailKeeper);
+        //result3 = cV.searchEmailByDate()
+
+        // awh@malcolmhardie.com
+        // 12/12/2004
+        // 12/12/2006
+        // test
+
+        System.out.println("\n" + result1.size() + "\n");
+        System.out.println(result1);
+
+        System.out.println("\n" + result2.size() + "\n");
+        System.out.println(result2);
+
+        System.out.println("\n" + eMailKeeper.size() + "\n");
         System.out.println(eMailKeeper);
+        System.out.println(eMailKeeper.get(1).getData());
 
     }
 }
