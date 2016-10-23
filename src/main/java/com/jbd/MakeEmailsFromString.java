c4427bbbf8f2778fa9aa90110fb5b1df58bafff9
Kacper <kacper.rabczak@gmail.com>
2016-10-23 13:50:50 +0200

    deleted comments and basic refactoring

--- src/main/java/com/jbd/MakeEmailsFromString.java
+++ src/main/java/com/jbd/MakeEmailsFromString.java
@@ -10,40 +10,26 @@ public class MakeEmailsFromString {
     private String EMAILREGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     private String FROMREGEX = "(from\\s)("+EMAILREGEX+")";
     private String SUBJECTREGEX = "(subject:\\s)(.*)";
-    //private String DATEREGEX = "(\\ndate:\\s)(.*)";
-    //private String CONTENTREGEX = "(x-list.*|list.*)(\\n\\n)(?s)(.*)(\\n\\n\\n)";//(?s)<FMSFlightPlan>(.*?)</FMSFlightPlan>
+    private String DATEREGEX = "(\\ndate:\\s)(.*)";
+    //private String CONTENTREGEX = "(x-list.*|list.*)(\\n\\n)(?s)(.*)(\\n\\n\\n)";//(?s)<yolo>(.*?)</yolo>
 
-    public List<Email> makeEmailList(String file){
-        Matcher fromMatcher = getMatcherFor(FROMREGEX, file);
-        Matcher subjectMatcher = getMatcherFor(SUBJECTREGEX, file);
+    public List<Email> makeEmailList(String fromString){
+        Matcher fromMatcher = getMatcherFor(FROMREGEX, fromString);
+        Matcher subjectMatcher = getMatcherFor(SUBJECTREGEX, fromString);
+        Matcher dateMatcher = getMatcherFor(DATEREGEX, fromString);
+        //Matcher contentMatcher = getMatcherFor(CONTENTREGEX, fromString);
 
-        //Pattern pattern3 = Pattern.compile(DATEREGEX, Pattern.CASE_INSENSITIVE);
-        //Matcher matcher3 = pattern3.matcher(file);
-
-        //Pattern pattern4 = Pattern.compile(CONTENTREGEX, Pattern.CASE_INSENSITIVE);
-        //Matcher matcher4 = pattern4.matcher(file);
-
-        while(fromMatcher.find() && subjectMatcher.find()){
-            Email e = new Email(fromMatcher.group(2),subjectMatcher.group(2));
+        while(fromMatcher.find() && subjectMatcher.find() && dateMatcher.find()){
+            Email e = new Email(fromMatcher.group(2),subjectMatcher.group(2),dateMatcher.group(2), "add contentMatcher when ready");
             emailsFromString.add(e);
         }
-
-//        while(matcher4.find()) {
-//            System.out.println("found: "
-//                    + matcher4.start() + " - " + matcher4.end());
-//            System.out.println(matcher4.group(3));
-//        }
-
-//        for(int i=0;i<1500;i++){
-//            System.out.print(file.charAt(i));
-//        }
 
         return emailsFromString;
     }
 
-    private Matcher getMatcherFor(String fromregex, String file) {
+    private Matcher getMatcherFor(String fromregex, String fromString) {
         Pattern pattern = Pattern.compile(fromregex, Pattern.CASE_INSENSITIVE);
-        return pattern.matcher(file);
+        return pattern.matcher(fromString);
     }
 
 
