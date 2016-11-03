2c916184b847af7a615447e67b1ec40e92a6c770
Marcin Bartoszek <marbar1812@gmail.com>
2016-11-03 12:15:17 +0100

    Add adding numbers when there is no duplicates Corrected regex to phonePatternWithPlus (now it searching phone number like : "+48515444666", "+48 515444666", "515444666"

--- src/main/java/com/jbd/KeywordsFinder/DisplayPhoneNumbers.java
+++ src/main/java/com/jbd/KeywordsFinder/DisplayPhoneNumbers.java
@@ -10,8 +10,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DisplayPhoneNumbers {
-    String test = " 24 +48 +48 515417846 515417846 135487562314";
-    String phonePatternWithPlus = "(\\s\\+?\\d{2})(\\s\\d{9}\\b)"; // +48 515417888
+    //String test = " 24 +48 +48 515417846 515417846 135487562314";
+    String phonePatternWithPlus = "(\\s\\+?\\d{2}?)?(\\s\\d{9}\\b)"; // +48 515417888
     //String phonePatternWithoutSpace = "(\\s\\+?\\d{2})(\\d{9}\\b)"; // +48515417888
     String phonePatternWithoutPlusAndSpacs = "(\\b\\d{9}\\b)"; // 515417888
     private Pattern patternWithPlus;
@@ -21,7 +21,6 @@ public class DisplayPhoneNumbers {
     private boolean searchDuplicates(Map<String, List<String>> mapToSearch, String stringTooSearch) {
         boolean foundDuplicate = false;
         for (String key : mapToSearch.keySet()) {
-            System.out.println(key);
             for (String value : mapToSearch.get(key)) {
                 if (value.equals(stringTooSearch)) {
                     foundDuplicate = true;
@@ -34,18 +33,20 @@ public class DisplayPhoneNumbers {
         return foundDuplicate;
     }
 
-    private String returnFormattedPhoneNumber(String phoneNumber){
+    private String returnFormattedPhoneNumber(String phoneNumber) {
         String formatedPhoneNumber;
-        formatedPhoneNumber = phoneNumber.trim().replace(" ","");
-        return  formatedPhoneNumber;
+        formatedPhoneNumber = phoneNumber.trim().replace(" ", "");
+        return formatedPhoneNumber;
     }
 
     public Map<String, List<String>> searchPhoneNumbers(List<Email> emailList) {
         Map<String, List<String>> resultMap = new TreeMap<>();
         List<String> testArrayList = new ArrayList<>();
+        List<String> listContainingPhoneNumbers = new ArrayList<>();
         patternWithPlus = Pattern.compile(phonePatternWithPlus);
         patternWithoutPlusAndSpaces = Pattern.compile(phonePatternWithoutPlusAndSpacs);
         // patternWithoutSpace = Pattern.compile(phonePatternWithoutSpace);
+        boolean isDuplicate = false;
 
         for (Email email : emailList) {
             Matcher matcher = patternWithPlus.matcher(email.getContent());
@@ -54,19 +55,22 @@ public class DisplayPhoneNumbers {
             while (matcher.find()) {
                 System.out.println("Matcher 0");
                 System.out.println(matcher.group());
+                System.out.println("----------------------");
                 String phoneNumber = returnFormattedPhoneNumber(matcher.group());
-                System.out.println("Uzyto funckji" + phoneNumber);
-//                String phoneNumber = matcher.group();
-//                System.out.println(phoneNumber.replace(" ", ""));
-//                String fromEmail = email.getFrom();
-//                testArrayList.add(phoneNumber);
-//                resultMap.put(fromEmail, testArrayList);
-//                break;
+                System.out.println("Uzyto funckji " + phoneNumber);
+                testArrayList.add("+48515417844");
+                resultMap.put("marcin@wp.pl", testArrayList);
+                isDuplicate = searchDuplicates(resultMap, phoneNumber);
+                System.out.println(isDuplicate);
+                if (!isDuplicate) {
+                    System.out.println("Brak duplikatów: dodaje numer");
+                    listContainingPhoneNumbers = resultMap.get("marcin@wp.pl");
+                    System.out.println("Lista maili: " + listContainingPhoneNumbers);
+                    resultMap.put(email.getFrom(), testArrayList);
+                }
+//
             }
-//            while (matcher2.find()) {
-//                System.out.println("Matcher 2");
-//                System.out.println(matcher2.group());
-//            }
+
             while (matcher1.find()) {
                 System.out.println("Matcher 1");
                 System.out.println(matcher1.group(0));
