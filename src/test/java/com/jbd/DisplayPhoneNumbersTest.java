5bea4353c09711567036bb435ebd76ca0428ee4f
Cinek1812 <marbar1812@gmail.com>
2016-11-07 16:41:56 +0100

    Add new test case checking correct working Duplicate() function

--- src/test/java/com/jbd/DisplayPhoneNumbersTest.java
+++ src/test/java/com/jbd/DisplayPhoneNumbersTest.java
@@ -13,19 +13,29 @@ import static org.junit.Assert.*;
  * Created by Admin on 07.11.2016.
  */
 public class DisplayPhoneNumbersTest {
-    List<Email> eMailKeeper =  Arrays.asList(new Email("marcin@wp.pl","subject","2015-01-01","test 515-417-846"),
-            new Email("marcin@wp.pl","subject","2015-01-01","test 515417844"),
-            new Email("marcin@wp.pl","subject","2015-01-01","test 515 417 846"),
-            new Email("wojtek@wp.pl","subject","2015-01-01","test 616478366"),
-            new Email("wojtek@wp.pl","subject","2015-01-01","test 616478366"));
+    List<Email> eMailKeeper = Arrays.asList(new Email("marcin@wp.pl", "subject", "2015-01-01", "test 515-417-846"),
+            new Email("marcin@wp.pl", "subject", "2015-01-01", "test 515417844"),
+            new Email("marcin@wp.pl", "subject", "2015-01-01", "test 515 417 846"),
+            new Email("wojtek@wp.pl", "subject", "2015-01-01", "test 616478366"),
+            new Email("wojtek@wp.pl", "subject", "2015-01-01", "test 616478366"));
+
+    private Map<String, List<String>> resultMap;
+    private List<String> resultList;
+    DisplayPhoneNumbers displayPhoneNumbers = new DisplayPhoneNumbers();
+
     @Test
-    public void shouldReturnListSize2() throws Exception {
-        Map<String,List<String>> resultMap;
-        List<String> resultList;
-        DisplayPhoneNumbers displayPhoneNumbers = new DisplayPhoneNumbers();
+    public void forTwoDifferentNumberAndOneTheSameShouldReturnListSize2() throws Exception {
         resultMap = displayPhoneNumbers.searchPhoneNumbers(eMailKeeper);
         resultList = resultMap.get("marcin@wp.pl");
         assertThat("Size of the list number is not equal 2! ", resultList.size(), is(2));
     }
+
+    @Test
+    public void forTheSamePhoneNumberShouldReturnSizeEqual1() throws Exception {
+        resultMap = displayPhoneNumbers.searchPhoneNumbers(eMailKeeper);
+        resultList = resultMap.get("wojtek@wp.pl");
+        assertThat("Add the same phone number to List! Should be unique", resultList.size(), is(1));
+    }
+
 
 }
