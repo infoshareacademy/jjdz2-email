a9ec09c6ddd4e4c7ec84deeecd28cac0f47c0bfb
Cinek1812 <marbar1812@gmail.com>
2016-11-07 17:06:50 +0100

    Refactor classes

--- src/main/java/com/jbd/DisplayPhoneNumbers.java
+++ src/main/java/com/jbd/DisplayPhoneNumbers.java
@@ -1,7 +1,5 @@
 package com.jbd;
 
-import com.jbd.Email;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -10,15 +8,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DisplayPhoneNumbers {
-    //String test = " 24 +48 +48 515417846 515417846 135487562314";
-//    String phonePatternWithPlus = "(\\s\\+?\\d{2}?)?(\\s\\d{9}\\b)"; // +48 515417888
-    String phonePatternWithPlus = "(\\s\\d{3}[\\-,\\s]?\\d{3}[\\-,\\s]?\\d{3}\\b)"; // +48 515417888
+    String phonePattern = "(\\s\\d{3}[\\-,\\s]?\\d{3}[\\-,\\s]?\\d{3}\\b)"; // 515417888
 
-    //String phonePatternWithoutSpace = "(\\s\\+?\\d{2})(\\d{9}\\b)"; // +48515417888
-    String phonePatternWithoutPlusAndSpacs = "(\\b\\d{9}\\b)"; // 515417888
-    private Pattern patternWithPlus;
-    private Pattern patternWithoutPlusAndSpaces;
-    private Pattern patternWithoutSpace;
+    private Pattern patternPhone;
 
     private boolean searchDuplicates(Map<String, List<String>> mapToSearch, String stringTooSearch) {
         boolean foundDuplicate = false;
@@ -26,6 +18,7 @@ public class DisplayPhoneNumbers {
             for (String value : mapToSearch.get(key)) {
                 if (value.trim().equals(stringTooSearch)) {
                     foundDuplicate = true;
+                    break;
                 }
             }
             if (foundDuplicate)
@@ -38,55 +31,39 @@ public class DisplayPhoneNumbers {
     private String returnFormattedPhoneNumber(String phoneNumber) {
         String formatedPhoneNumber;
         formatedPhoneNumber = phoneNumber.trim().replace(" ", "");
-        //TO DO!
-        //zamienic - na nic
         if (formatedPhoneNumber.contains("-")) {
-            formatedPhoneNumber = phoneNumber.replace("-", "");
-            formatedPhoneNumber.trim();
+            formatedPhoneNumber = phoneNumber.trim().replace("-", "");
         }
         return formatedPhoneNumber;
     }
 
     public Map<String, List<String>> searchPhoneNumbers(List<Email> emailList) {
         Map<String, List<String>> resultMap = new TreeMap<>();
-        List<String> testArrayList = new ArrayList<>();
-        List<String> listContainingPhoneNumbers = new ArrayList<>();
-        patternWithPlus = Pattern.compile(phonePatternWithPlus);
-        patternWithoutPlusAndSpaces = Pattern.compile(phonePatternWithoutPlusAndSpacs);
-        // patternWithoutSpace = Pattern.compile(phonePatternWithoutSpace);
+        patternPhone = Pattern.compile(phonePattern);
         boolean isDuplicate = false;
 
         for (Email email : emailList) {
-            Matcher matcher = patternWithPlus.matcher(email.getContent());
-            Matcher matcher1 = patternWithoutPlusAndSpaces.matcher(email.getContent());
-            // Matcher matcher2 = patternWithoutSpace.matcher(email.getContent());
+            Matcher matcher = patternPhone.matcher(email.getContent());
             while (matcher.find()) {
-                System.out.println("Poczatek!-----------------------------------");
-                System.out.println("Matcher 0");
-                System.out.println(matcher.group());
                 String phoneNumber = returnFormattedPhoneNumber(matcher.group());
                 System.out.println("Uzyto funckji: " + phoneNumber);
                 if (resultMap.isEmpty()) {
                     System.out.println("Mapa pusta dodaje rekord");
                     List<String> phoneList = new ArrayList<>();
-                    //phoneNumber = returnFormattedPhoneNumber(phoneNumber);
                     phoneList.add(phoneNumber.trim());
                     resultMap.put(email.getFrom(), phoneList);
-                    System.out.println("Dodano : " + resultMap.get(email.getFrom()) + " od " + email.getFrom());
-                } else {
+                    } else {
                     if (resultMap.containsKey(email.getFrom())) {
                         System.out.println("Mapa zawiera element " + email.getFrom());
                         System.out.println("Szukam czy duplikat...");
                         isDuplicate = searchDuplicates(resultMap, phoneNumber);
                         if (!isDuplicate) {
                             System.out.println("Brak duplikatów: dodaje numer");
-                            List<String> tempPhoneNumberList = new ArrayList<>();
-                            tempPhoneNumberList = resultMap.get(email.getFrom());
-                            tempPhoneNumberList.add(phoneNumber);
-                            testArrayList = tempPhoneNumberList;
-                            //listContainingPhoneNumbers = resultMap.get("marcin@wp.pl");
-                            System.out.println("Lista maili: " + testArrayList);
-                            resultMap.put(email.getFrom(), testArrayList);
+                            List<String> phoneNumberList;
+                            phoneNumberList = resultMap.get(email.getFrom());
+                            phoneNumberList.add(phoneNumber);
+                            System.out.println("Lista maili: " + phoneNumberList);
+                            resultMap.put(email.getFrom(), phoneNumberList);
                         } else {
                             System.out.println("Duplikat, nie dodaje!");
                         }
