452c3aab7f951379b827955589cc8e3f6d8ad424
Michał Piechota <mchpiechota@gmail.com>
2017-02-03 15:28:16 +0100

    Added all tests needed for FiveDaysNoAnswer class

--- src/test/java/com/jbd/FiveDaysNoAnswerTest.java
+++ src/test/java/com/jbd/FiveDaysNoAnswerTest.java
@@ -1,4 +1,57 @@
 package com.jbd;
 
-public class FiveDaysNoAnswerTest {
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class FiveDaysNoAnswerTest extends FiveDaysNoAnswer {
+    List<FiveDaysNoAnswer> fdnal = Arrays.asList();
+    public List<Date> sortedEmailDatesInDate = new ArrayList<>();
+
+    public List<LocalDateTime> afterAllList = new ArrayList<>();
+    FiveDaysNoAnswer fdna1 = new FiveDaysNoAnswer();
+    LocalDateTime sixDaysAgo = LocalDateTime.now().minusDays(6);
+
+
+    @Test
+    public void shouldReturnTrueIfListIsEmpty() throws Exception {
+
+        boolean sut = fdna1.chceckIfContentBetween(false);
+        assertThat("List is not empty", sut, is(true));
+    }
+
+    @Test
+    public void schouldRemoveDatesFromListWhenItsBeforeFiveDaysAgo() throws Exception {
+        afterAllList.add(sixDaysAgo);
+
+        fdna1.checkIfWasAnswer(afterAllList);
+        if (afterAllList.size() == 0) {
+            afterAllList = null;
+        }
+
+        assertNull(afterAllList);
+
+    }
+
+    @Test
+    public void schouldRemoveWeekendsFromList() throws Exception {
+
+        Date saturday = new Date(777777777);
+        Date sunday = new Date(888888888);
+        sortedEmailDatesInDate.add(saturday);
+        sortedEmailDatesInDate.add(sunday);
+        fdna1.erasingFreeDaysFromDates(sortedEmailDatesInDate);
+        if (sortedEmailDatesInDate.size() == 0) {
+            sortedEmailDatesInDate = null;
+        }
+        assertNull(sortedEmailDatesInDate);
+
+    }
+
 }
